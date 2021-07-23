28305475a070332815b82e071e63abe884083bb9
EngelkenRyan <engelkenryan@gmail.com>
2021-07-22 23:31:21 -0400

    added password

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/Final`, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 });
